46e2acfc1901b46fa576517a403e341d23225188
JoséFilho <josefilhoredesocial@gmail.com>
2024-05-07 21:59:06 -0300

    Ajeitando o Js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     document.querySelector('.menu-opener').addEventListener("click", function() {
-        if(document.querySelector(".menu nav ul").style.display == 'flex') {
-            document.querySelector(".menu nav ul").style.display = 'none'
+        if(document.querySelector("nav").style.display == 'flex') {
+            document.querySelector("nav").style.display = 'none'
         } else {
-            document.querySelector(".menu nav ul").style.display = 'flex'
+            document.querySelector("nav").style.display = 'flex'
         }
     })
 }
